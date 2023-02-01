443c232a3218918b0c465ce4a17f717e6f73ae11
Paxxs <10364775+Paxxs@users.noreply.github.com>
2023-02-02 00:23:53 +0800

    chore: 使用 gulp 构建和压缩 js/html

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,47 @@
 'use strict';
 
-const { parallel, series } = require('gulp');
+const { src, dest, parallel, series } = require('gulp'),
+  rename = require("gulp-rename"),
 
-const gulp = require('gulp'),
   sass = require('gulp-sass')(require('sass')),
   autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
-  rename = require("gulp-rename"),
   cleanCSS = require('gulp-clean-css'),
   header = require('gulp-header'),
   pkg = require('./package.json'),
   banner = `
 /*!
  * LiteWebChat_Frame ${pkg.version} (${pkg.homepage})
- * MorFans Lab(c) 2017-${new Date().getFullYear()} ${pkg.author}
+ * MorFans Lab(c) 2017-${new Date().getFullYear()}
  * Licensed under ${pkg.license}
  */
-`.trim();
+`.trim(),
+
+  pug = require('gulp-pug'),
+  pangu = require('gulp-pangu'),
+
+  babel = require('gulp-babel'),
+  terser = require('gulp-terser'),
+
+  clean = require('gulp-clean'),
+  imagemin = require('gulp-imagemin');
+
+const paths = {
+  src_css: "./src/css",
+  src_js: "./src/js",
+  src_pug: "./src/pug",
+  src_images: "./src/images/**/*",
+
+  dist_images: "./dist/images",
+  dist_html: "./dist/html",
+  dist_css: "./dist/css",
+  dist_js: "./dist/js",
+}
 
 function css(path, newName) {
-  return gulp.src(path)
+  return src(path)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
     .pipe(header(banner))
     .pipe(autoprefixer())
     .pipe(rename({
@@ -29,13 +49,13 @@ function css(path, newName) {
       extname: ".css"
     }))
     .pipe(sourcemaps.write(`./map`))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(dest(paths.dist_css))
 }
 
 function cssMin(path, newName) {
-  return gulp.src(path)
+  return src(path)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
     .pipe(header(banner))
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: 'ie11' }))
@@ -44,40 +64,122 @@ function cssMin(path, newName) {
       extname: ".min.css"
     }))
     .pipe(sourcemaps.write('./map'))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(dest(paths.dist_css))
 }
 
-const buildLiteChat = () => {
-  return css('./src/css/index.scss', 'litewebchat')
-};
-const buildLiteChatMin = () => {
-  return cssMin('./src/css/index.scss', 'litewebchat')
-};
-
-const buildLiteChatInput = () => {
-  return css('./src/css/chatinput.scss', 'litewebchat_input')
+function js(path, newName) {
+  return src(path)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(header(banner))
+    .pipe(rename({
+      basename: newName,
+      extname: ".js"
+    }))
+    .pipe(sourcemaps.write('./map'))
+    .pipe(dest(paths.dist_js))
 }
-const buildLiteChatInputMin = () => {
-  return cssMin('./src/css/chatinput.scss', 'litewebchat_input')
+
+function jsMin(path, newName) {
+  return src(path)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(terser())
+    .pipe(header(banner))
+    .pipe(rename({
+      basename: newName,
+      extname: ".min.js"
+    }))
+    .pipe(sourcemaps.write('./map'))
+    .pipe(dest(paths.dist_js))
 }
 
+const buildLiteChat = (callback) => {
+  css(paths.src_css + '/index.scss', 'litewebchat');
+  cssMin(paths.src_css + '/index.scss', 'litewebchat');
+  callback();
+},
 
-// export.buildLiteChat
+  buildLiteChatInput = (cb) => {
+    css(paths.src_css + '/chatinput.scss', 'litewebchat_input');
+    cssMin(paths.src_css + '/chatinput.scss', 'litewebchat_input');
+    cb();
+  },
 
-// const buildLiteChat = css('./src/index.scss', 'litewebchat'),
-//   buildLiteChatMini = cssMin('./src/index.scss', 'litewebchat');
+  buildHtml = (cb) => {
+    // 这里不压缩 html, 防止其他使用者不能接受激进的 html 编写方式,但又需要方便的查看代码
+    src(paths.src_pug + '/*.pug')
+      .pipe(pug({
+        locals: {},
+        // pretty: true,
+      }))
+      .pipe(pangu())
+      .pipe(dest(paths.dist_html));
+    cb();
+  },
 
-// const buildLiteChatInput = css('./src/chatinput.scss', 'litewebchat_input'),
-//   buildLiteChatInputMini = cssMin('./src/chatinput.scss', 'litewebchat_input');
+  buildRenderJs = (cb) => {
+    js(paths.src_js + '/render.js', 'litewebchat_render');
+    jsMin(paths.src_js + '/render.js', 'litewebchat_render');
+    cb();
+  },
 
+  buildInputJs = (cb) => {
+    js(paths.src_js + '/input.js', 'litewebchat_input');
+    jsMin(paths.src_js + '/input.js', 'litewebchat_input');
+    cb();
+  },
+
+  minImage = (cb) => {
+    src(paths.src_images)
+      .pipe(imagemin())
+      .pipe(dest(paths.dist_images));
+    cb();
+  }
+
+
+  ;
+
+function cleanFiles() {
+  return src('./dist', { read: false })
+    .pipe(clean({ force: true }));
+}
+
+/**
+ * build css
+ */
 exports.buildLiteChat = buildLiteChat;
-exports.buildLiteChatMin = buildLiteChatMin;
 exports.buildLiteChatInput = buildLiteChatInput;
-exports.buildLiteChatInputMin = buildLiteChatInputMin;
 
-exports.buildCss = series(
-  parallel(buildLiteChat, buildLiteChatMin),
-  parallel(buildLiteChatInput, buildLiteChatInputMin)
+/**
+ * build html
+ */
+exports.buildHtml = buildHtml;
+
+/**
+ * build js
+ */
+exports.buildRenderJs = buildRenderJs;
+exports.buildInputJs = buildInputJs;
+
+/**
+ * build all
+ */
+exports.build = series(
+  parallel(buildLiteChat, buildLiteChatInput),
+  parallel(buildRenderJs, buildInputJs),
+  parallel(minImage, buildHtml),
 );
 
-// exports.default
+exports.rebuild = series(
+  cleanFiles,
+  parallel(buildLiteChat, buildLiteChatInput),
+  parallel(buildRenderJs, buildInputJs),
+  parallel(minImage, buildHtml),
+);
+
+exports.default = exports.build;
---
+++ src/js/render.js
@@ -0,0 +1,66 @@
+const TipsType = {
+  tipsNormal: 'tips',
+  tipsPrimary: 'tips-primary',
+  tipsSuccess: 'tips-success',
+  tipsInfo: 'tips-info',
+  tipsWarning: 'tips-warning',
+  tipsDanger: 'tips-danger',
+};
+const TitleType = {
+  admin: 'admin',
+  owner: 'owner',
+};
+
+function beforeRenderingHTML(data, chatboxClass) {
+  let htmlStr = '';
+  let chatBox = document.querySelector(chatboxClass);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].isRender) {
+      continue;
+    }
+    if (data[i].messageType.indexOf('tips') !== -1) {
+      htmlStr += renderTipHtml(data[i].html, TipsType[data[i].messageType] || 'tips');
+    } else {
+      htmlStr += renderMessageHtml(data[i]);
+    }
+    data[i].isRender = true;
+  }
+  chatBox.innerHTML += htmlStr;
+  setTimeout(() => {
+    if (chatBox.scrollHeight > chatBox.clientHeight) {
+      chatBox.scrollTop = chatBox.scrollHeight;
+      chatBox = '';
+      htmlStr = '';
+    }
+  }, 300);
+}
+
+function renderMessageHtml(data) {
+  return `<div class="c${data.position} cmsg">
+        <img class="headIcon ${data.diamond ? '' : 'radius'}" src="${data.headIcon}" ondragstart="return false;" oncontextmenu="return false;"/>
+        <span class="name">
+            ${renderTitleHtml(data.htitle, TitleType[data.htitleType] || '')}
+            <span>${escapeHtml(data.name) || '&nbsp;'}</span>
+        </span>
+        <span class="content">${data.messageType === 'raw' ? data.html : escapeHtml(data.html)}</span>
+    </div>`;
+}
+
+function renderTitleHtml(content, css) {
+  if (!content) return '';
+  return `<span class="htitle ${css}" style="margin: 0 4px 0 0;">${content}</span>`;
+}
+
+function renderTipHtml(content, css) {
+  if (!content) return '';
+  return `<div class="tips"><span class="${css}" style="margin-bottom: 20px;">${escapeHtml(content)}</span></div>`;
+}
+
+// 转义 C0 Controls and Basic Latin 中非数字和字母，C1 Controls and Latin-1 Supplement 全部
+// https://www.w3schools.com/charsets/ref_html_utf8.asp
+function escapeHtml(unsafe) {
+  return unsafe.replace(
+    /[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u00FF]/g,
+    c => '&#' + ('000' + c.charCodeAt(0)).slice(-4) + ';'
+  )
+}
